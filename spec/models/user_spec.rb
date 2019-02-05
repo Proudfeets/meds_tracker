7ebad1ff928aaff5fd36fe029859b86d36d6e18b
PROUDFEETS <sgabisouza@gmail.com>
2019-02-05 11:32:20 -0500

    Adds columns to user table to pass tests in user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "User model" do
+    it { should have_valid(:encrypted_password).when("Rogers")}
+    it { should_not have_valid(:encrypted_password).when("")}
+
+    it { should have_valid(:name).when("Michelle")}
+    it { should_not have_valid(:name).when("")}
+
+    it { should have_valid(:email).when("michelle@gmail.com")}
+    it { should_not have_valid(:email).when("arg.com")}
+
+
+  end
 end

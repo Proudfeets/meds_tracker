7981614f61d48c509bbe02d1cfc4c9ff638c99fd
PROUDFEETS <sgabisouza@gmail.com>
2019-02-06 17:07:35 -0500

    adds medications table and model with model tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_06_185018) do
+ActiveRecord::Schema.define(version: 2019_02_06_213739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "medications", force: :cascade do |t|
+    t.string "generic_name", default: "", null: false
+    t.string "brand_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+medications1 = Medication.create(generic_name: 'Warfarin', brand_name: 'Coumadin')
+medications2 = Medication.create(generic_name: 'Lorazepam', brand_name: 'Ativan')
+medications3 = Medication.create(generic_name: 'Fluoxetine', brand_name: 'Prozac')
+
+users1 = User.create(email: 'jack@kennedy.com', password: '123456')
+users2 = User.create(email: 'lizzie@warren.com', password: '123456')
+users3 = User.create(email: 'marty@walsh.com', password: '123456')

4a1439ca4527fcfe2de12975f602c964ea986e6b
PROUDFEETS <sgabisouza@gmail.com>
2019-02-11 20:07:54 -0500

    add prescription controller3

--- app/javascript/react/components/app.js
+++ app/javascript/react/components/app.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import {Route, browserHistory, IndexRoute, Router} from 'react-router';
-import UserShowContainer from '../containers/UserShowContainer'
+import UserShowContainer from '../containers/UserShowContainer';
 
 
 export const App = (props) => {
-  return (
+  return(
     <Router history={browserHistory}>
       <Route path="/" component={UserShowContainer}>
       </Route>

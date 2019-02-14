1fcc36b3d1976fb8148ddf80e56de0f26f42e1df
PROUDFEETS <sgabisouza@gmail.com>
2019-02-14 15:21:03 -0500

    narrows down code, re-adds file

--- app/serializers/prescription_serializer.rb
+++ app/serializers/prescription_serializer.rb
@@ -1,7 +1,6 @@
 class PrescriptionSerializer < ActiveModel::Serializer
   attributes :id, :user, :medication, :medication_brand, :medication_generic, :dosage, :frequency_number, :frequency_period, :special_instructions, :prescribed_by
 
-
   def instructions
     object.special_instructions
   end

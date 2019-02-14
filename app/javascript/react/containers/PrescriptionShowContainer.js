3a8257b562d807d6e2f2c045938b243a72a49ae8
PROUDFEETS <sgabisouza@gmail.com>
2019-02-14 15:30:55 -0500

    re-adds prescriptionShowContainer

---
+++ app/javascript/react/containers/PrescriptionShowContainer.js
@@ -0,0 +1,20 @@
+class PrescriptionSerializer < ActiveModel::Serializer
+  attributes :id, :user, :medication, :medication_brand, :medication_generic, :dosage, :frequency_number, :frequency_period, :special_instructions, :prescribed_by
+
+  def instructions
+    object.special_instructions
+  end
+
+  def prescriber
+    object.prescribed_by
+  end
+
+  def medication_generic
+    object.medication.generic_name
+  end
+
+  def medication_brand
+    object.medication.brand_name
+  end
+
+end

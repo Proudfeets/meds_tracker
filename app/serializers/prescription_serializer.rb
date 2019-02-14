e0a028475f0c7c7ee82a3536f4bef025be434d8c
PROUDFEETS <sgabisouza@gmail.com>
2019-02-14 14:44:23 -0500

    remove extra functions from serializer

--- app/serializers/prescription_serializer.rb
+++ app/serializers/prescription_serializer.rb
@@ -1,28 +1,12 @@
 class PrescriptionSerializer < ActiveModel::Serializer
   attributes :id, :user, :medication, :dosage, :frequency_number, :frequency_period, :special_instructions, :prescribed_by
 
-  def dosage
-    object.dosage
-  end
-
-  def frequency_number
-    object.frequency_number
-  end
-
-  def frequency_period
-    object.frequency_period
-  end
-
   def instructions
     object.special_instructions
   end
 
   def prescriber
     object.prescribed_by
-  end
-
-  def user_name
-    object.user.name
   end
 
   def medication_generic
